32b585f0903c9eb7896e1fa5385c3809cd073e05
GeekTynan <44474574+GeekTynan@users.noreply.github.com>
2026-08-01 15:04:14 +0800

    feat: 添加 vercel.json 配置并适配端口

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 // 让前端页面可以被访问
 app.use(express.static('public'));
 
